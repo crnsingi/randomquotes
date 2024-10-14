593539ba16a8d8fa7fb9ecc79968b8c48fa9fcf8
Cesar <cesarnsingi@hotmail.com>
2024-10-14 08:12:17 +0400

    js set

--- script.js
+++ script.js
@@ -1,51 +1,20 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
 const api_url = "https://api.quotable.io/random";
 
 async function getquote(url){
     const response = await fetch(url);
     var data = await response.json();
-    console.log(data);
+
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+
+}
+
+getquote(api_url);
+
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "---- by " + author.innerHTML, "Tweet Window", "width=600, height=300");
 }
 
 
-/*
-
-    "The only way to do great work is to love what you do. - Steve Jobs",
-   
-    "Innovation distinguishes between a leader and a follower. - Steve Jobs",
-   
-    "Don't be afraid to give up the good to go for the great. - John D. Rockefeller",
-   
-    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-   
-    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
-   
-   ]; // array of quotes 
-   
-   
-   
-   const generateButton = document.getElementById("generateButton");
-   
-   const quoteElement = document.getElementById("quote"); //event listener to the "random Quote" button
-   
-   
-   generateButton.addEventListener("click", generateRandomQuote);
-    // function to generate the random quote
-   
-   function generateRandomQuote() {  // Generate a random index within the range of the quotes array
-   
-   
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-     // Display the randomly selected quote in the quoteElement
-   
-   
-   
-    quoteElement.textContent = quotes[randomIndex];
-   
-   } */
-
-    
-   
-   
-   
-   
-   
