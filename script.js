0b7b78ef5d6f5a9482e3f7c6bb96188a3f36e979
Cesar <cesarnsingi@hotmail.com>
2024-10-14 08:19:24 +0400

    api not loading

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 const api_url = "https://api.quotable.io/random";
 
-async function getquote(url){
+async function getquote(url) {
     const response = await fetch(url);
-    var data = await response.json();
-
+    const data = await response.json();
     quote.innerHTML = data.content;
     author.innerHTML = data.author;
-
 }
 
 getquote(api_url);
 
-function tweet(){
-    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "---- by " + author.innerHTML, "Tweet Window", "width=600, height=300");
+function tweet() {
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + " ---- by " + author.innerHTML, "Tweet Window", "width=600, height=300");
 }
-
-
